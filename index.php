52bdaf642dc72adeecd67533e815110c8c8772fe
TruongSon248 <sn8835302@gmail.com>
2025-11-23 17:36:53 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$controller = isset($_GET['controller']) ? $_GET['controller'] : 'tour';
-$action = isset($_GET['action']) ? $_GET['action'] : 'index';
-
-if ($controller == 'tour') {
-    require_once "./controllers/TourController.php";
-    $ctr = new TourController();
-    $ctr->$action();
-}
